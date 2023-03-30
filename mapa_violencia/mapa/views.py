8369616f55a7b70062a8584d98c6d521962fa037
Vítor Dresch <dreschv@gmail.com>
2023-03-30 17:03:33 -0300

    Flag para tipo de crime.

--- mapa_violencia/mapa/views.py
+++ mapa_violencia/mapa/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import folium
 
 def mapa(request):
 
-    return render(request, 'mapa_violencia/index.html')
+    tipo_crime = 'all'
+
+    if request.POST:
+        tipo_crime = request.POST['tipo_crime']
+
+    m = folium.Map(location=[45.5236, -122.6750], zoom_start=13)
+
+    # Add a marker to the map
+    folium.Marker(
+        location=[45.5236, -122.6750],
+        popup='Portland, Oregon',
+        icon=folium.Icon(icon='cloud')
+    ).add_to(m)
+
+    # Render the map in the template
+    map_html = m._repr_html_()
+
+
+
+    return render(request, 'mapa_violencia/index.html', {'map_html': map_html, 'tipo_crime': tipo_crime})
