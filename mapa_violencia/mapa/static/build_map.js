1b18a11ad11e5940331b3a101610781ae58dc219
Vítor Dresch <dreschv@gmail.com>
2023-05-28 17:13:34 -0300

    Got the map to work with the geometry stored on the db.

--- mapa_violencia/mapa/static/build_map.js
+++ mapa_violencia/mapa/static/build_map.js
@@ -1,28 +1,6 @@
 var map = L.map('map', {zoomSnap: 0.1}).setView([-30.096859, -51.152677], 10.6);
 
-//Neighborhoods shape
-var porto_alegre = JSON.parse(JSON.parse(document.getElementById('geojson').textContent));
-
-//Filter only seleced neighborhoods
-var teste = $.ajax({
-    url: "return_filters/", // if you don't have dynamic url,
-    type: 'GET',
-  });
-
-console.log(teste)
-
-function neighborhoods_filter(feature) {
-    if (feature.properties.Name === "AGRONOMIA") return true
-  }
-
-console.log(porto_alegre)
-
 function onEachFeature(feature, layer) {
-    // does this feature have a property named popupContent?
-    console.log(feature.properties)
-    if (feature.properties && feature.properties.popupContent) {
-        layer.bindPopup(feature.properties.popupContent);
-    }
     layer.on({
         mouseover: highlightFeature,
         mouseout: resetHighlight,
@@ -46,10 +24,52 @@ function resetHighlight(e) {
     geojson.resetStyle(e.target);
 }
 
-geojson = L.geoJSON(porto_alegre, {
-    filter: neighborhoods_filter,
-    onEachFeature: onEachFeature
-}).addTo(map);
+function concatGeoJSON(g1, g2){
+    return { 
+        "type" : "FeatureCollection",
+        "features": g1.features.concat(g2)
+    }
+}
+
+//Get selected neighborhoods
+
+function getData() { 
+    return $.ajax({
+      url: "return_filters",
+      type: 'GET',
+    });
+  };
+
+async function getNeighborhoods() {
+    try {
+        var neighborhods = await getData();
+
+        var g1 = { "type" : "FeatureCollection",
+        "features" : []};
+
+        for (var i = 0; i < neighborhods.lista_bairros.length; i++){
+        // console.log(neighborhods.lista_bairros[i])
+            g1 = concatGeoJSON(g1, neighborhods.lista_bairros[i].geometry);
+        }
+
+        console.log(g1)
+
+        geojson = L.geoJSON(g1, {
+            //filter: neighborhoods_filter,
+            onEachFeature: onEachFeature
+        }).addTo(map);
+      
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+getNeighborhoods();
+
+
+function neighborhoods_filter(feature) {
+    if (feature.properties.Name === "AGRONOMIA") return true
+  }
 
 //Layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
--- mapa_violencia/mapa/views.py
+++ mapa_violencia/mapa/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from pathlib import Path
 from mapa.models import Bairro
-import json
 
 def mapa(request):
-
-    src = Path("resources/shapesbairros2016/poa.geojson")
-
-    geojson = json.dumps(json.loads(src.read_text()))
 
     lista_crimes = ['Teste1', 'Teste2', 'Teste3']
     lista_bairros = ['Teste1', 'Teste2', 'Teste3']
 
-    context = {'lista_crimes': lista_crimes, 'lista_bairros': lista_bairros, 'geojson': geojson}
+    context = {'lista_crimes': lista_crimes, 'lista_bairros': lista_bairros}
 
     return render(request, 'mapa_violencia/index.html', context)
 
@@ -21,12 +15,10 @@ def mapa(request):
 def return_filters(request):
 
     all_entries = Bairro.objects.all()
-    print(all_entries.values())    
+    #print(all_entries.values())    
 
     # Process the request and prepare the response
     response_data = {
         'lista_bairros': list(all_entries.values()),
-        'message': 'Function called successfully!',
-        'data': 'Any data you want to send back to the client',
     }
     return JsonResponse(response_data)
