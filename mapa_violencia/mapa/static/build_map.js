0317049dd02e4fc2dc92b2a514feccc2e717fe54
Vítor Dresch <dreschv@gmail.com>
2023-05-28 09:52:35 -0300

    SOme progress with the map

--- mapa_violencia/mapa/static/build_map.js
+++ mapa_violencia/mapa/static/build_map.js
@@ -1,32 +1,54 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map', {zoomSnap: 0.1}).setView([-30.096859, -51.152677], 10.6);
+
+//Neighborhoods shape
+var porto_alegre = JSON.parse(JSON.parse(document.getElementById('geojson').textContent));
+
+console.log(porto_alegre)
+
+function onEachFeature(feature, layer) {
+    // does this feature have a property named popupContent?
+    console.log(feature.properties)
+    if (feature.properties && feature.properties.popupContent) {
+        layer.bindPopup(feature.properties.popupContent);
+    }
+}
+
+L.geoJSON(porto_alegre, {
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+//Layer
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var drawnItems = new L.FeatureGroup();
-map.addLayer(drawnItems);
-var drawControl = new L.Control.Draw({
-    // position: 'topright',
-    draw: {
-       polygon: {
-        shapeOptions: {
-         color: 'purple'
-        },
-        allowIntersection: true,
-        drawError: {
-         color: 'orange',
-         timeout: 1000
-        },
-       },
-       polyline: {},
-       rect: {},
-       circle: {},
-    },
+
+//Draw
+// var drawnItems = new L.FeatureGroup();
+// map.addLayer(drawnItems);
+
+// var drawControl = new L.Control.Draw({
+//     // position: 'topright',
+//     draw: {
+//        polygon: {
+//         shapeOptions: {
+//          color: 'purple'
+//         },
+//         allowIntersection: true,
+//         drawError: {
+//          color: 'orange',
+//          timeout: 1000
+//         },
+//        },
+//        polyline: {},
+//        rect: {},
+//        circle: {},
+//     },
     
-});
-map.addControl(drawControl);
- map.on('draw:created', function (e) {
-       var type = e.layerType,
-           layer = e.layer;
-       drawnItems.addLayer(layer);
-});
+// });
+// map.addControl(drawControl);
+//  map.on('draw:created', function (e) {
+//        var type = e.layerType,
+//            layer = e.layer;
+//        drawnItems.addLayer(layer);
+// });
--- mapa_violencia/mapa/views.py
+++ mapa_violencia/mapa/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from pathlib import Path
+import json
 
 def mapa(request):
+
+    src = Path("resources/shapesbairros2016/poa.geojson")
+
+    geojson = json.dumps(json.loads(src.read_text()))
 
     lista_crimes = ['Teste1', 'Teste2', 'Teste3']
     lista_bairros = ['Teste1', 'Teste2', 'Teste3']
 
-    context = {'lista_crimes': lista_crimes, 'lista_bairros': lista_bairros}
+    context = {'lista_crimes': lista_crimes, 'lista_bairros': lista_bairros, 'geojson': geojson}
 
     return render(request, 'mapa_violencia/index.html', context)
 
